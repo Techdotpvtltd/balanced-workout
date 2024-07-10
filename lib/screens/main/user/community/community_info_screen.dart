a1f98fa3cc798cb97825428f6cf22e7552eb8e91
Ali Akbar <akbarstudyinfo786@gmail.com>
2024-07-10 22:17:16 +0500

    Redeesign community screen

--- lib/screens/main/user/community/community_info_screen.dart
+++ lib/screens/main/user/community/community_info_screen.dart
@@ -5,10 +5,15 @@
 // Date:        09-05-24 16:08:51 -- Thursday
 // Description:
 
+import 'package:balanced_workout/app/app_manager.dart';
 import 'package:balanced_workout/blocs/chat/%20chat_bloc.dart';
 import 'package:balanced_workout/blocs/chat/chat_event.dart';
 import 'package:balanced_workout/models/chat_model.dart';
+import 'package:balanced_workout/models/user_model.dart';
+import 'package:balanced_workout/screens/components/custom_container.dart';
 import 'package:balanced_workout/screens/components/custom_dropdown.dart';
+import 'package:balanced_workout/screens/main/user/community/add_member_screens.dart';
+import 'package:balanced_workout/utils/extensions/navigation_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
@@ -37,6 +42,8 @@ class _CommunityInfoScreenState extends State<CommunityInfoScreen> {
   final TextEditingController descriptionController = TextEditingController();
   final TextEditingController maxController = TextEditingController();
   String? avatar;
+  final UserModel? user = AppManager().user;
+  late final bool isAdmin = user?.uid == chat.createdBy;
 
   void triggerUpdateProfileEvent() {
     context.read<ChatBloc>().add(
@@ -160,6 +167,97 @@ class _CommunityInfoScreenState extends State<CommunityInfoScreen> {
             titleText: "Max Community Limit",
             hintText: 'Add number of persons can join.',
             keyboardType: TextInputType.number,
+          ),
+
+          gapH22,
+
+          /// Members Detail Screen
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "${chat.participantUids.length} ${chat.participantUids.length > 1 ? "Members" : "Member"}",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              if (isAdmin)
+                IconButton(
+                  onPressed: () {
+                    NavigationService.go(const AddMemberScreen());
+                  },
+                  icon: const Icon(
+                    Icons.add,
+                    color: AppTheme.primaryColor1,
+                  ),
+                )
+            ],
+          ),
+          gapH10,
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: const BoxDecoration(
+              color: AppTheme.darkWidgetColor2,
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+            ),
+            child: Column(
+              children: [
+                for (int i = 0; i < chat.participants.length; i++)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: CustomContainer(
+                      onPressed: () {},
+                      color: const Color(0xFF232323).withOpacity(0.62),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 9),
+                      borderRadius: const BorderRadius.all(Radius.circular(50)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          /// Custom Avatar
+                          Expanded(
+                            child: Row(
+                              children: [
+                                AvatarWidget(
+                                  width: 41,
+                                  height: 41,
+                                  backgroundColor: Colors.black,
+                                  avatarUrl: chat.participants[i].avatarUrl,
+                                ),
+                                gapW10,
+
+                                /// Name Text
+                                Flexible(
+                                  child: Text(
+                                    chat.participants[i].uid == user?.uid
+                                        ? "You"
+                                        : chat.participants[i].name,
+                                    maxLines: 1,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          // CustomMenuDropdown(
+                          //   items: const [],
+                          //   onSelectedItem: (val, index) {},
+                          // )
+                        ],
+                      ),
+                    ),
+                  ),
+              ],
+            ),
           ),
         ],
       ),
