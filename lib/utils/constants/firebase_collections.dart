f60c09278372f781f5ea017f3c60d6af7f65c4e0
Ali Akbar <akbarstudyinfo786@gmail.com>
2024-10-29 00:00:04 +0500

    tweak

--- lib/utils/constants/firebase_collections.dart
+++ lib/utils/constants/firebase_collections.dart
@@ -6,11 +6,11 @@ const String FIREBASE_COLLECTION_USER =
     "${kReleaseMode ? "Rel-" : "Dev-"}Users";
 const String FIREBASE_COLLECTION_USER_PROFILES =
     "${kReleaseMode ? "Rel-" : "Dev-"}Avatars";
-const FIREBASE_COLLECTION_PLANS = "${kReleaseMode ? "Rel-" : "Rel-"}Plans";
+const FIREBASE_COLLECTION_PLANS = "${kReleaseMode ? "Rel-" : "Dev-"}Plans";
 const FIREBASE_COLLECTION_WORKOUTS =
-    "${kReleaseMode ? "Rel-" : "Rel-"}Workouts";
+    "${kReleaseMode ? "Rel-" : "Dev-"}Workouts";
 
-const FIREBASE_COLLECTION_COURSE = "${kReleaseMode ? "Rel-" : "Rel-"}Course";
+const FIREBASE_COLLECTION_COURSE = "${kReleaseMode ? "Rel-" : "Dev-"}Course";
 
 const String FIREBASE_COLLECTION_CHAT =
     "${kReleaseMode ? "Rel-" : "Dev-"}Chats";
@@ -19,10 +19,10 @@ const FIREBASE_COLLECTION_MESSAGES =
     "${kReleaseMode ? "Rel-" : "Dev-"}Messages";
 
 const FIREBASE_COLLECTION_ARTICLES =
-    "${kReleaseMode ? "Rel-" : "Rel-"}Articles";
+    "${kReleaseMode ? "Rel-" : "Dev-"}Articles";
 
 const FIREBASE_COLLECTION_LOG_WORKOUTS =
-    "${kReleaseMode ? "Rel-" : "Rel-"}Log-Workouts";
+    "${kReleaseMode ? "Rel-" : "Dev-"}Log-Workouts";
 
 const FIREBASE_COLLECTION_LOG_EXERCISES =
-    "${kReleaseMode ? "Rel-" : "Rel-"}Log-Exercises";
+    "${kReleaseMode ? "Rel-" : "Dev-"}Log-Exercises";
